d9f429775de6d4f2a5f06612a581d824669928e1
Karbozov Damir <damirkap89@mail.ru>
2021-01-20 12:47:21 +0600

    make it better

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import  StaticFiles
 
-app.mount("/static", StaticFiles(directory="static"), name="static")
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 url = 'https://api.adviceslip.com/advice'
